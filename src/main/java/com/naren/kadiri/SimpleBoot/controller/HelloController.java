2c6f53840168eab4d57006bd1849a7e46683c6ec
Narendra <nkadiri09@gmail.com>
2020-07-31 18:19:35 -0400

    another chagne

--- src/main/java/com/naren/kadiri/SimpleBoot/controller/HelloController.java
+++ src/main/java/com/naren/kadiri/SimpleBoot/controller/HelloController.java
@@ -1,19 +1,27 @@
 package com.naren.kadiri.SimpleBoot.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
         @RequestMapping("/")
         public String index() {
             return "Greetings from Spring Boot!";
         }
 
-    @RequestMapping("/hello")
-    public String sayHello(@RequestParam String name) {
-        return "Greetings from Spring Boot!"+name;
-    }
+        @RequestMapping("/hello")
+        public String sayHello(@RequestParam String name) {
+            return "Greetings from Spring Boot!"+name;
+        }
+
+        @RequestMapping("{name}")
+        public String requestValues(@PathVariable String name) {
+            return "Greetings from "+name;
+        }
+
 
     }
