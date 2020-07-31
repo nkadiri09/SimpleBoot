88379cf06449550f96c4b2a951656ce3f57f2ef3
Narendra <nkadiri09@gmail.com>
2020-07-31 17:02:32 -0400

    adding Hello endi=point with name, Id, and age

--- src/main/java/com/naren/kadiri/SimpleBoot/controller/HelloController.java
+++ src/main/java/com/naren/kadiri/SimpleBoot/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.naren.kadiri.SimpleBoot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
         @RequestMapping("/")
         public String index() {
             return "Greetings from Spring Boot!";
+        }
+
+        @RequestMapping("/hello")
+        public String addAll(@RequestParam String name, @RequestParam String id,
+                             @RequestParam String age ) {
+            return "Greetings from to "+name+" "+id+" "+age;
         }
 
     }
