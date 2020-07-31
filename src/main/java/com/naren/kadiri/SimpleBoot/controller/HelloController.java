20b326d77ebac9f99bb4f844eb43506a244a6d41
Narendra <nkadiri09@gmail.com>
2020-07-31 16:43:46 -0400

    first commit

---
+++ src/main/java/com/naren/kadiri/SimpleBoot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.naren.kadiri.SimpleBoot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+        @RequestMapping("/")
+        public String index() {
+            return "Greetings from Spring Boot!";
+        }
+
+    }
---
+++ src/test/java/com/naren/kadiri/SimpleBoot/SimpleBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naren.kadiri.SimpleBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
